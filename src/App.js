52cad80a705d343140f1ac1c80f438490c3c1bf7
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-13 20:10:48 +0530

    practicing useEffect hooks

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 import HooksCounter2 from "./component/useState/HooksCounter2";
 import HooksObject from "./component/useState/HooksObject";
 import HooksArrays from "./component/useState/HooksArrays";
+import Effect from "./component/classEquiv/Effect";
+import EffectsIntro from "./component/useEffect/EffectsIntro";
 
 function App() {
   return (
     <div className='App'>
-      <HooksArrays />
+      <Effect />
+      <EffectsIntro />
+      {/* <HooksArrays /> */}
       {/* <HooksObject /> */}
       {/* <HooksCounter2 /> */}
     </div>
